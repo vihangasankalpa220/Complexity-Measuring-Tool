52399dd670d94c08632c118219759a37d2b96930
Chinthaka Samarakoon <47790015+ChinthakaSam@users.noreply.github.com>
2019-08-10 21:24:09 +0530

    update comments

--- tool/CodeInGrams/src/com/codeingrams/inheritance/Iinheritance.java
+++ tool/CodeInGrams/src/com/codeingrams/inheritance/Iinheritance.java
@@ -2,7 +2,7 @@ package com.codeingrams.inheritance;
 
 import java.io.IOException;
 
-//interface for interface for geting values reagrding inheritance
+//interface for geting values reagrding inheritance
 public interface Iinheritance {
 
 	public void count(String path) throws IOException;
