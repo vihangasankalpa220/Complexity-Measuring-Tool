df6b9cc0cf3a3ae5cae86cdfec95a918d82d80fc
vihanga <48063915+vihangasankalpa220@users.noreply.github.com>
2019-09-20 05:39:06 -0700

    Add files via upload

--- tool/CodeInGrams/src/com/codeingrams/size/ISize.java
+++ tool/CodeInGrams/src/com/codeingrams/size/ISize.java
@@ -3,5 +3,8 @@ package com.codeingrams.size;
 import java.io.IOException;
 
 public interface ISize {
-	public void count(String path) throws IOException;
+	public   void MeasureSize() throws IOException;
+
+
+	
 }
